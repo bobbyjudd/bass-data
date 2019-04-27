a21643f51ca7f9a1eda7afb76e96d69aa00f8446
Bobby Judd <bobbymjudd@gmail.com>
2019-04-26 21:27:38 -0700

    Convert built-in data structures to Redis API

--- bass/site_crawler.py
+++ bass/site_crawler.py
@@ -2,31 +2,41 @@ import urllib.request
 from urllib.error import HTTPError
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
+import redis
 
-visited = set()
-tournament_urls = set()
+r = redis.Redis(
+    host='localhost',
+    port=6379)
+
 BASE_URL = "https://www.bassmaster.com/"
 T_URL = "/results/tournaments"
-url_stack = [BASE_URL]
 
-while url_stack:
-    print(url_stack[-1])
-    visited.add(url_stack[-1])
-    if url_stack[-1].startswith(T_URL) and url_stack[-1] not in tournament_urls:
-        tournament_urls.add(url_stack[-1])
+def valid_search_url(search_url):
+    return (search_url and
+            search_url.startswith('/') and
+            not search_url.startswith('/search') and
+            not r.sismember('visited', search_url))
+
+r.rpush('url_stack', BASE_URL)
+
+while r.llen('url_stack'):
+    url = r.rpop('url_stack').decode('utf-8')
+    print(url)
+    r.sadd('visited', url)
+    if url.startswith(T_URL) and not r.sismember('tournament_urls', url):
+        r.sadd('tournament_urls', url)
         with open("t_urls.txt" , "a") as f:
-            f.write(url_stack[-1]+'\n')
-
+            f.write(url+'\n')
     try:
-        r = urllib.request.urlopen(urljoin(BASE_URL, url_stack[-1]))
+        req = urllib.request.urlopen(urljoin(BASE_URL, url))
     except:
         continue
     finally:
-        url_stack.pop()
+        r.rpop('url_stack')
 
-    page = r.read()
+    page = req.read()
     soup = BeautifulSoup(page, 'html.parser')
     for link in soup.find_all('a'):
-        url = link.get('href')
-        if url and url.startswith('/') and not url.startswith('/search') url not in visited:
-            url_stack.append(url)
+        soup_url = link.get('href')
+        if valid_search_url(soup_url):
+            r.rpush('url_stack', soup_url)
